fe9eec1b89e4e448de78fce3265746be62ddb687
Atharva-Kanerkar <atharvakanherkar25@gmail.com>
2025-08-25 03:53:25 +0530

    Added stuff

--- app/main.py
+++ app/main.py
@@ -64,7 +64,9 @@ app = FastAPI(
 # CORS middleware
 allowed_origins = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://pulse-seven-blue.vercel.app",
+    "https://pulse-seven-blue.vercel.app/"
 ]
 
 app.add_middleware(
